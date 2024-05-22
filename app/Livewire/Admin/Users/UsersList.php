dc843cd75aa423d6d729035e565b6c4f0f3d610f
manimohamadi <mohamadimahdi@yahoo.com>
2024-05-23 01:05:37 +0330

    add upload image

--- app/Livewire/Admin/Users/UsersList.php
+++ app/Livewire/Admin/Users/UsersList.php
@@ -3,16 +3,102 @@
 namespace App\Livewire\Admin\Users;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
+use Livewire\Features\SupportFileUploads\WithFileUploads;
 use Livewire\WithPagination;
 
 class UsersList extends Component
 {
-    use WithPagination;
+    use WithPagination, WithFileUploads;
     protected $paginationTheme = 'bootstrap';
+
+    public $name;
+    public $email;
+    public $password;
+    public $mobile;
+    public $search;
+    public $editUser = null;
+
+    public $img;
+
     public function render()
     {
-        $users = User::paginate(1);
-        return view('livewire.admin.users.users-list',compact('users'))->layout('admin.layouts.master');
+        if ($this->search and mb_strlen($this->search) > 1) {
+            $users = User::where('name', 'like', '%' . $this->search . '%')->paginate(10);
+        } else {
+            $users = User::paginate(10);
+        }
+        return view('livewire.admin.users.users-list', compact('users'))->layout('admin.layouts.master');
+    }
+
+    public function mount()
+    {
+    }
+
+    public function edit($userId)
+    {
+        $user = User::find($userId);
+        $this->name = $user->name;
+        $this->email = $user->email;
+        $this->mobile = $user->mobile;
+        $this->editUser = $user->id;
+    }
+
+    public function saveEdit($userId)
+    {
+        $this->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email,' . $userId,
+            'mobile' => 'required|digits:11',
+            // 'password' => 'required|min:6',
+            // 'img' => 'image'
+        ]);
+
+        $user = User::find($userId);
+        $user->name = $this->name;
+        $user->email = $this->email;
+        $user->mobile = $this->mobile;
+
+        if ($user->save()) {
+            $this->reset('name', 'email', 'password', 'mobile', 'img');
+            session()->flash('success', 'با موفقیت ویرایش شد');
+        } else {
+            session()->flash('error', 'مشکل در ویرایش');
+        }
+
+        $this->editUser = null;
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'mobile' => 'required|digits:11',
+            'img' => 'image'
+        ]);
+
+        $name = '';
+        if ($this->img) {
+            $name = time() . rand(10, 99) . '.' .  $this->img->getClientOriginalExtension();
+            $this->img->storeAs('userImages', $name, 'public');
+        }
+
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
+            'mobile' => $this->mobile,
+            'img' => $name,
+        ]);
+
+        if ($user) {
+            $this->reset('name', 'email', 'password', 'mobile', 'img');
+            session()->flash('success', 'با موفقیت ثبت شد');
+        } else {
+            session()->flash('error', 'مشکل در ثبت');
+        }
     }
 }
--- resources/views/livewire/admin/users/users-list.blade.php
+++ resources/views/livewire/admin/users/users-list.blade.php
@@ -9,26 +9,51 @@
             <div class="container">
                 <h6 class="card-title">ایجاد کاربر</h6>
 
-                <form method="POST">
+                <form method="POST" wire:submit="update">
                     <div class="row">
+
+                        <div class="col-md-12">
+                            <div class="row">
+
+                                @if ($message = session()->get('success'))
+                                    <div class="alert alert-success">
+                                        <span>{{ $message }}</span>
+                                    </div>
+                                @endif
+                                @if ($message = session()->get('error'))
+                                    <div class="alert alert-danger">
+                                        <span>{{ $message }}</span>
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
 
                         <div class="col-md-6">
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label">نام و نام خانوادگی</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control text-left" dir="rtl" name="name">
+                                    <input type="text" class="form-control text-left" dir="rtl" wire:model="name">
+                                    @error('name')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label">ایمیل</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control text-left" dir="rtl" name="email">
+                                    <input type="text" class="form-control text-left" dir="rtl" wire:model="email">
+                                    @error('email')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label">موبایل</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control text-left" dir="rtl" name="mobile">
+                                    <input type="text" class="form-control text-left" dir="rtl" wire:model="mobile">
+                                    @error('mobile')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
@@ -37,19 +62,41 @@
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label">پسورد</label>
                                 <div class="col-sm-8">
-                                    <input type="text" class="form-control text-left" dir="rtl" name="password">
+                                    <input type="text" class="form-control text-left" dir="rtl" wire:model="password">
+                                    @error('password')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label" for="file"> آپلود عکس </label>
-                                <input class="col-sm-8" type="file" class="form-control-file" id="file">
+                                <input class="col-sm-6" type="file" class="form-control-file" wire:model="img" id="file">
+                                @error('img')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+
+                                @if ($img)
+                                    <div class="col-md-2">
+                                        <img style="float: left;width:60px;max-height:60px;" src="{{ $img->temporaryUrl() }}" alt="">
+                                    </div>
+                                @endif
                             </div>
                             <div class="form-group row">
                                 <label class="col-sm-4 col-form-label" for=""></label>
                                 <div class="col-sm-8">
-                                    <button type="submit" class="btn btn-success  btn-uppercase">
-                                        <i class="ti-check-box m-r-5"></i> ذخیره
-                                    </button>
+                                    @if ($editUser == null)
+                                        <button type="submit" class="btn btn-success  btn-uppercase" wire:loading.remove>
+                                            <i class="ti-check-box m-r-5"></i>ایجاد
+                                        </button>
+                                    @else
+                                        <button type="submit" class="btn btn-success  btn-uppercase" wire:click="saveEdit({{$editUser}})" wire:loading.remove>
+                                            <i class="ti-check-box m-r-5"></i>ویرایش
+                                        </button>
+                                    @endif
+
+
+                                    <div wire:loading>loading...</div>
+
                                 </div>
                             </div>
                         </div>
@@ -64,7 +111,7 @@
                 <div class="form-group row">
                     <label class="col-sm-2 col-form-label">عنوان جستجو</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control text-left" dir="rtl" wire:model="search">
+                        <input type="text" class="form-control text-left" dir="rtl" wire:model.live="search">
                     </div>
                 </div>
                 <table class="table table-striped table-hover">
@@ -87,7 +134,7 @@
                                 <td class="text-center align-middle"> {{ $key }}</td>
                                 <td class="text-center align-middle">
                                     <figure class="avatar avatar">
-                                        <img src="{{ $user->img }}" class="rounded-circle" alt="image">
+                                        <img src="{{ asset('userImages/' . $user->img) }}" class="rounded-circle" alt="image">
                                     </figure>
                                 </td>
                                 <td class="text-center align-middle">{{ $user->name }}</td>
@@ -97,9 +144,11 @@
                                     <span class="cursor-pointer badge badge-success">{{ $user->status == 'active' ? 'فعال' : 'غیر فعال' }}</span>
                                 </td>
                                 <td class="text-center align-middle">
-                                    <a class="btn btn-outline-info" href="#">
-                                        ویرایش
-                                    </a>
+                                    @if ($editUser !== $user->id)
+                                        <a class="btn btn-outline-info" href="#" wire:click="edit({{ $user->id }})">
+                                            ویرایش
+                                        </a>
+                                    @endif
                                 </td>
                                 <td class="text-center align-middle">{{ $user->created_at }}</td>
                             </tr>
